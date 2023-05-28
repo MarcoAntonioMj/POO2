e028a45209246674a8225a3412b6c8a8bb91e7df
Marco Antonio <marcotonho38@gmail.com>
2023-05-28 17:54:08 -0300

    feat : Adicionando classes no Passgaeiro

--- marco/src/main/java/com/atividade1/modelo/Passageiro.java
+++ marco/src/main/java/com/atividade1/modelo/Passageiro.java
@@ -1,5 +1,28 @@
 package com.atividade1.modelo;
 
+import java.util.Scanner;
+
 public class Passageiro {
+    private String nome ;
+    private int cpf;
+
+ public void coletaNome(Scanner scanner){
+    nome = scanner.nextLine();
+ }
+ public void coletaCpf(Scanner scanner){
+    cpf = scanner.nextInt();
+ }
+public String getNome() {
+    return nome;
+}
+public void setNome(String nome) {
+    this.nome = nome;
+}
+public int getCpf() {
+    return cpf;
+}
+public void setCpf(int cpf) {
+    this.cpf = cpf;
+}
 
 }
