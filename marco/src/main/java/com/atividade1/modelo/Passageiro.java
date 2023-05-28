895ee845baea35a78a37b7364bbafb86a0c71c4c
Marco Antonio <marcotonho38@gmail.com>
2023-05-28 18:01:01 -0300

    feat : arrumando logica classe pesso e voo

--- marco/src/main/java/com/atividade1/modelo/Passageiro.java
+++ marco/src/main/java/com/atividade1/modelo/Passageiro.java
@@ -3,26 +3,41 @@ package com.atividade1.modelo;
 import java.util.Scanner;
 
 public class Passageiro {
-    private String nome ;
+    private String nome;
     private int cpf;
 
- public void coletaNome(Scanner scanner){
-    nome = scanner.nextLine();
- }
- public void coletaCpf(Scanner scanner){
-    cpf = scanner.nextInt();
- }
-public String getNome() {
-    return nome;
-}
-public void setNome(String nome) {
-    this.nome = nome;
-}
-public int getCpf() {
-    return cpf;
-}
-public void setCpf(int cpf) {
-    this.cpf = cpf;
+    public void coletaNome(Scanner scanner) {
+        nome = scanner.nextLine();
+    }
+
+    public void coletaCpf(Scanner scanner) {
+        cpf = scanner.nextInt();
+        scanner.nextLine();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public void solicitarDados() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome do passageiro:");
+        coletaNome(scanner);
+        System.out.println("Digite o CPF do passageiro:");
+        coletaCpf(scanner);
+        scanner.close();
+    }
 }
 
-}
--- marco/src/main/java/com/atividade1/modelo/Voo.java
+++ marco/src/main/java/com/atividade1/modelo/Voo.java
@@ -1,5 +1,47 @@
 package com.atividade1.modelo;
 
 public class Voo {
+    private int numero;
+    private String origem;
+    private String destino;
+    private int capacidade;
 
+    public Voo(int numero, String origem, String destino, int capacidade) {
+        this.numero = numero;
+        this.origem = origem;
+        this.destino = destino;
+        this.capacidade = capacidade;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
 }
