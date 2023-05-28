bf884b795f051c7831ecf9a5538588ea745f9051
Marco Antonio <marcotonho38@gmail.com>
2023-05-28 18:02:42 -0300

    feat : Criando classe Reserva

--- marco/src/main/java/com/atividade1/modelo/Reserva.java
+++ marco/src/main/java/com/atividade1/modelo/Reserva.java
@@ -1,5 +1,38 @@
 package com.atividade1.modelo;
 
 public class Reserva {
+    private Voo voo;
+    private Passageiro passageiro;
+    private String assento;
 
+    public Reserva(Voo voo, Passageiro passageiro, String assento) {
+        this.voo = voo;
+        this.passageiro = passageiro;
+        this.assento = assento;
+    }
+
+    public Voo getVoo() {
+        return voo;
+    }
+
+    public void setVoo(Voo voo) {
+        this.voo = voo;
+    }
+
+    public Passageiro getPassageiro() {
+        return passageiro;
+    }
+
+    public void setPassageiro(Passageiro passageiro) {
+        this.passageiro = passageiro;
+    }
+
+    public String getAssento() {
+        return assento;
+    }
+
+    public void setAssento(String assento) {
+        this.assento = assento;
+    }
 }
+
